41c88553d1c85223a8a8bfb780863204a4bc6af2
filbert288 <filbert.tedja6@gmail.com>
2025-12-17 21:35:14 +0700

    Update restaurants.php

--- restaurants.php
+++ restaurants.php
@@ -14,30 +14,27 @@ $vendors = mysqli_query($conn, "
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>LocalFarmer</title>
+    <title>Restaurants</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
 </head>
 <body class="restaurants-page">
-
-    <header>
-        <div class="logo">
-            <img src="PAWHUB.png" alt="Logo">
-            <a href="home.php">Home</a>
+    <nav>
+        <img src="assets/PAWHUB.png" alt="Logo">
+        <div class="nav-left">
+            <a href="home_guest.php">Home</a>
+            <a href="cart.php">Cart</a>
+            <a href="logout.php">Logout</a>
         </div>
+        
+        <a class="login-btn">
+            <?php isset($_SESSION['name']) ?>
+                <?php echo htmlspecialchars($_SESSION['name']); ?>
+        </a>
+    </nav>
 
-        <?php if(!$isLogin): ?>
-            <a href="login.php" class="btn-login">Login</a>
-        <?php else: ?>
-            <div class="user-info">
-                <span>Hi, <?= htmlspecialchars($_SESSION['user_name']); ?></span>
-                <a href="logout.php" class="btn-login">Logout</a>
-            </div>
-        <?php endif; ?>
-    </header>
-
-
-    <h1>Restaurants</h1>
+    <h2>Restaurants</h2>
 
     <div class="cards">
 
@@ -48,49 +45,76 @@ $vendors = mysqli_query($conn, "
             <?php while($v = mysqli_fetch_assoc($vendors)): ?>
                 <a href="detail.php?id=<?= $v['id']; ?>" class="card-link">
                     <div class="card">
-                        <img src="assets/menu1.jpg">
+                        <img src="assets/NasiLoca.jpg">
                         <p>Nasi Loca Healthy Indonesia Rice Bowl by Mini Calore</p>
-                        <p>Kuningan</p>
+                        <br>
+                        <div class="location">
+                            <i class="fa-solid fa-location-dot"></i>
+                            <span>Kuningan</span>
+                        </div>
                     </div>
                 </a>
 
                 <a href="detail.php?id=<?= $v['id']; ?>" class="card-link">
                     <div class="card">
-                        <img src="assets/menu1.jpg">
+                        <img src="assets/Boost.jpg">
                         <p>Boost Juice Bars</p>
-                        <p>WTC Sudirman</p>
+                        <br>
+                        <br>
+                        <div class="location">
+                            <i class="fa-solid fa-location-dot"></i>
+                            <span>WTC Sudirman</span>
+                        </div>
                     </div>
                 </a>
 
                 <a href="detail.php?id=<?= $v['id']; ?>" class="card-link">
                     <div class="card">
-                        <img src="assets/menu1.jpg">
+                        <img src="assets/Shigeru.jpg">
                         <p>Shigeru Sushi</p>
-                        <p>Lippo Mall Puri</p>
+                        <br>
+                        <br>
+                        <div class="location">
+                            <i class="fa-solid fa-location-dot"></i>
+                            <span>Lippo Mall Puri</span>
+                        </div>
                     </div>
                 </a>
 
                 <a href="detail.php?id=<?= $v['id']; ?>" class="card-link">
                     <div class="card">
-                        <img src="assets/menu1.jpg">
-                        <p>Gado - Gado Boplo</p>
-                        <p>Cassablanca</p>
+                        <img src="assets/diet go.png">
+                        <p>Diet Go (Nasi Ayam Beef Healthy)</p>
+                        <br>
+                        <div class="location">
+                            <i class="fa-solid fa-location-dot"></i>
+                            <span>Kuningan</span>
+                        </div>
                     </div>
                 </a>
 
                 <a href="detail.php?id=<?= $v['id']; ?>" class="card-link">
                     <div class="card">
-                        <img src="assets/menu1.jpg">
-                        <p>Gado - Gado Boplo</p>
-                        <p>Cassablanca</p>
+                        <img src="assets/IG-2024-2.jpg">
+                        <p>Subway</p>
+                        <br>
+                        <br>
+                        <div class="location">
+                            <i class="fa-solid fa-location-dot"></i>
+                            <span>Lippo Mall Nusantara</span>
+                        </div>
                     </div>
                 </a>
 
                 <a href="detail.php?id=<?= $v['id']; ?>" class="card-link">
                     <div class="card">
-                        <img src="assets/menu1.jpg">
-                        <p>Gado -Gado Boplo</p>
-                        <p>Cassablanca</p>
+                        <img src="assets/c590dc9b-232c-4ee2-a1ea-5d22b3101cd8_gobiz-dashboard-image_1713933242794.jpg">
+                        <p>Greenly (Healthy Salad, Bowl, Juice)</p>
+                        <br>
+                        <div class="location">
+                            <i class="fa-solid fa-location-dot"></i>
+                            <span>Sudirman</span>
+                        </div>
                     </div>
                 </a>
             <?php endwhile; ?>
